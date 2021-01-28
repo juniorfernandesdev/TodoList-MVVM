608a79459130aa49982fa40988809488f250cff8
Junior Fernandes <juniorbass8@gmail.com>
2021-01-27 23:34:48 -0300

    Criado a VC de lista TODO

---
+++ Todo-List/Todo List/View/List/ListTodoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ListTodoViewController.swift
+//  Todo List
+//
+//  Created by Junior Fernandes on 25/01/21.
+//
+
+import UIKit
+
+class Nav: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+class ListTodoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Todo List"
+
+
+        // Do any additional setup after loading the view.
+    }
+}
